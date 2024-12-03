903eae75ae6206bfeede3f09c0275d1f776dcdfb
Season4Coffee <as8108865@gmail.com>
2024-12-03 23:28:58 +0300

    Create main.cpp

---
+++ Microwave/main.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  Microwave m1;
+  int work=0, command;
+  while (work){
+    cout << "Меню\n" << "1:Мощность слабая, время 1 мин\n" << "2:Мощность слабая, время 2 мин\n" << "3:Мощность средняя, время 1 мин\n" << "4:Мощность средняя, время 2 мин\n" << "5:Мощность высокая, время 1 мин\n" << "6:Мощность высокая, время 2 мин\n" << "7:Выключена\n";
+    cin >>command;
+    switch(command){
+      case 1:
+      break;
+      case 2:
+      break;
+      case 3:
+      break;
+      case 4:
+      break;
+      case 5:
+      break;
+      case 6:
+      break;
+      case 7: work=0;
+      break;
+    }
+  }
+}
