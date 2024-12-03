0a68695337d2a773d448d501a38764bdef9e8953
Season4Coffee <as8108865@gmail.com>
2024-12-03 23:08:21 +0300

    Create Microwave.h

---
+++ Microwave/Microwave.h
@@ -0,0 +1,15 @@
+#ifndef MICROWAVE_H
+#define MICROWAVE_H
+
+#include <bits/stdc++.h>
+using namespace std;
+class Microwave{
+public:
+Microwave();
+Microwave(x,y)
+int sttime();
+int sttemp();
+private:
+int x;
+int y;
+}
