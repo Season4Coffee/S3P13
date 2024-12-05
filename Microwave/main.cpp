cd9a423f764e0a5c43c3a66d585ffe349afcf293
Season4Coffee <as8108865@gmail.com>
2024-12-05 21:14:39 +0300

    Update main.cpp

--- Microwave/main.cpp
+++ Microwave/main.cpp
@@ -2,24 +2,41 @@
 using namespace std;
 int main(){
   Microwave m1;
+  int x;
+  int y;
   int work=0, command;
   while (work){
     cout << "Меню\n" << "1:Мощность слабая, время 1 мин\n" << "2:Мощность слабая, время 2 мин\n" << "3:Мощность средняя, время 1 мин\n" << "4:Мощность средняя, время 2 мин\n" << "5:Мощность высокая, время 1 мин\n" << "6:Мощность высокая, время 2 мин\n" << "7:Выключена\n";
     cin >>command;
     switch(command){
       case 1:
+        m1.sttime(1);
+        m1.sttemp(1);
       break;
       case 2:
+        m1.sttime(2);
+        m1.sttemp(1);
       break;
       case 3:
+        m1.sttime(1);
+        m1.sttemp(2);
       break;
       case 4:
+        m1.sttime(2);
+        m1.sttemp(2);
       break;
       case 5:
+        m1.sttime(1);
+        m1.sttemp(3);
       break;
       case 6:
+        m1.sttime(2);
+        m1.sttemp(3);
       break;
-      case 7: work=0;
+      case 7:
+        m1.sttime(0);
+        m1.sttemp(0);
+        work=0;
       break;
     }
   }
