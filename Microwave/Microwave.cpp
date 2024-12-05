5b4cb97967c12e5f3839c675a6e162e721ea2c83
Season4Coffee <as8108865@gmail.com>
2024-12-05 21:08:34 +0300

    Update Microwave.cpp

--- Microwave/Microwave.cpp
+++ Microwave/Microwave.cpp
@@ -1,5 +1,33 @@
 #include "Microwave.h"
 using namespace std;
 Microwave::Microwave(){
-  
+  time=0;
+  power=0;
+}
+Microwave::sttime(int x){
+  time=x;
+  cout << "Время работы" << " " << time <<" минуты.\n";
+  if(x==0){
+    vk=0;
+    cout << "Микроволновка выключена\n";
+  }
+}
+Microwave::sttemp(int y){
+  power=y;
+  if(power==1){
+    cout << "Мощность маленькая\n";
+  }
+  else if(power==2){
+    cout << "Мощность средняя\n";
+  }
+  else{
+    cout << "Мощность высокая\n";
+  }
+}
+
+Microwave::Microwave(int x,int y){
+  if(x<=3 & y<=3){
+    time=x;
+    power=y;
+  }
 }
